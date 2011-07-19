a251127a58318a88238256fe73122b4bec606bb5
jdejong <jdejong@sparknettech.com>
2011-07-19 12:10:25 -0400

    DOH! missed a spot.

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -15,7 +15,7 @@ Spree::BaseController.class_eval do
     # But we realy don't want to query DB on each page we're sure doesn't exist!
     return if Rails.cache.fetch('page_not_exist/'+request.path)
 
-    if @page = CmsPage.visible.find_by_slug(request.path)
+    if @page = CmsPage.visible.find_by_slug(request.path.chomp('.js'))
       if @page.layout && !@page.layout.empty?
         render :template => 'static_content/show', :layout => @page.layout
       else

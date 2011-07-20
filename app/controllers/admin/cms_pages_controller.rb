8f31001749e55b71c96a6e16db197ca495e61e4a
jdejong <jdejong@sparknettech.com>
2011-07-20 11:38:34 -0400

    fixed redirect errors

--- app/controllers/admin/cms_pages_controller.rb
+++ app/controllers/admin/cms_pages_controller.rb
@@ -6,8 +6,8 @@ class Admin::CmsPagesController < Admin::BaseController
   end
   
   update.after do
-    expire_page :controller => '/static_content', :action => 'show', :path => @page.slug
-    Rails.cache.delete('page_not_exist/'+@page.slug)
+    expire_page :controller => '/static_content', :action => 'show', :path => @cms_page.slug
+    #Rails.cache.delete('page_not_exist/'+@cms_page.slug)
   end
   
   create.response do |wants|
@@ -15,7 +15,7 @@ class Admin::CmsPagesController < Admin::BaseController
   end
 
   create.after do
-    Rails.cache.delete('page_not_exist/'+@page.slug)
+    #Rails.cache.delete('page_not_exist/'+@cms_page.slug)
   end
 
 end
